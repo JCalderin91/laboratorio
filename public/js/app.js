e30d51dd4480bb7d1b790ff87828e31c35e9de88
Jcalderin91 <jesuscalderin91@gmail.com>
2018-11-19 19:32:20 -0400

    Cambios menores

--- public/js/app.js
+++ public/js/app.js
@@ -1,30 +1,30 @@
-$('#disable').on('show.bs.modal',function(event){
-  let button = $(event.relatedTarget);
-  let id  = button.data('id');
-  let modal = $(this);
-  modal.find('.modal-body #id').val(id);
+$('#disable').on('show.bs.modal', function(event) {
+    let button = $(event.relatedTarget);
+    let id = button.data('id');
+    let modal = $(this);
+    modal.find('.modal-body #id').val(id);
 });
 
-$('#enable').on('show.bs.modal',function(event){
-  let button = $(event.relatedTarget);
-  let id  = button.data('id');
-  let modal = $(this);
-  modal.find('.modal-body #id').val(id);
+$('#enable').on('show.bs.modal', function(event) {
+    let button = $(event.relatedTarget);
+    let id = button.data('id');
+    let modal = $(this);
+    modal.find('.modal-body #id').val(id);
 });
 
-$('#hour').on('show.bs.modal',function(event){
-  let button = $(event.relatedTarget);
-  let id  = button.data('user_id');
-  let op  = button.data('operation');
-  let modal = $(this);
-  modal.find('.modal-body #user_id').val(id);
-  modal.find('.modal-body #user_id').val(id);
-  modal.find('.modal-body #operation').val(op);
+$('#hour').on('show.bs.modal', function(event) {
+    let button = $(event.relatedTarget);
+    let id = button.data('user_id');
+    let op = button.data('operation');
+    let modal = $(this);
+    modal.find('.modal-body #user_id').val(id);
+    modal.find('.modal-body #user_id').val(id);
+    modal.find('.modal-body #operation').val(op);
 });
 
 
 window.setTimeout(function() {
-  $(".alert").fadeTo(500, 0).slideUp(1000, function(){
-      $(this).remove();
-  });
+    $(".alert").fadeTo(500, 0).slideUp(1000, function() {
+        $(this).remove();
+    });
 }, 5000);
