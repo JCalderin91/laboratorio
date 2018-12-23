e8c1aab555d52d5c275a976a2f5485b364b97843
Jcalderin91 <jesuscalderin91@gmail.com>
2018-12-23 08:48:22 -0400

    Vista de registro de ordenes cambio del title de app.blade.php

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -4,7 +4,7 @@
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
 
-  <title>Laravel</title>
+  <title>Laboratorio</title>
   <link rel="stylesheet" href="{{ asset('css/font-awesome/css/all.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('css/app.css') }}">
