bd4f7ab7e41c824cf93bff22379981bb4e75e712
jesus <jesuscalderin91@gmail.com>
2019-01-21 09:55:23 -0400

    Cambios menores en alert de entrega de orden

--- resources/js/components/views/Dashboard.vue
+++ resources/js/components/views/Dashboard.vue
@@ -313,7 +313,7 @@
             Swal({
               type: 'success',
               title: 'Excelente',
-              html:'<p>Datos guardados con exito</p><h3>Código de orden: <strong>LAB-'+response.data.data.codigo+'</strong></h3><small class="text-danger">Recuerde solicitar la firma de este código</small>',
+              html:'<p>Datos guardados con exito</p><h3>Código de orden: <strong>LAB-'+response.data.data.codigo+'</strong></h3><small class="text-danger font-weight-bold">Recuerde solicitar la firma de este código</small>',
               confirmButtonText: 'Continuar',
             }).then(() => {
               this.$router.push('/')
