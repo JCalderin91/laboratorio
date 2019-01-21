ac99d1c96fd2f86e8f9c479ca50fdf72e4b6c15f
Muse19 <wendyhurtado95@gmail.com>
2019-01-21 12:33:51 -0400

    actualizar usuario - corregido

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -50,15 +50,9 @@ class UserController extends ApiController{
     }
 
 
-    public function update(UserUpdateRequest $request, $id){
+    public function update(UserUpdateRequest $request, User $user){
         
-        $user = User::findOrFail($id);
-
-        $user->fill($request->except(['password']));
-
-        if($request->has('password')){
-            $user->password = bcrypt($request->password);
-        }
+        $user->fill($request->all());
 
         if(!$user->isDirty()){
             return $this->errorResponse('Se debe especificar al menos un valor diferente para actualizar' , 422);     
