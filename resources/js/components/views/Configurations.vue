e8171dd9d1f58f4f1f2f6a9442630e7704b31994
Miguel Maestre <maestre.95@gmail.com>
2019-01-22 23:23:55 -0400

    dobleclick para editar contraseñas

--- resources/js/components/views/Configurations.vue
+++ resources/js/components/views/Configurations.vue
@@ -1,47 +1,69 @@
 <template>
-	<div class="card col-11" style="margin: 10px;">
-		<div class="card-content row">
+  <div class="card col-11" style="margin: 10px;">
+    <div class="card-content row">
 
-			<div class="card-title col-12 p-0">
-				<h4 >Configuraciones del sistema</h4>
-			</div>
-			<div class="card-body p-0">
-				<table class="table">
-        	<thead class="thead-dark">
-        		<tr>
-        			<th>Tipo de usuario</th>
-        			<th>Contraseña</th>
-        			<th>Acción</th>
-        		</tr>
-        	</thead>
-        	<tbody>
-        		<tr>
-        			<td>Profesor</td>
-        			<td>*********</td>
-        			<td></td>
-        		</tr>
-        		<tr>
-        			<td>Técnico</td>
-        			<td>*********</td>
-        			<td></td>
-        		</tr>
-        	</tbody>
+      <div class="card-title col-12 p-0">
+        <h4 >Configuraciones del sistema</h4>
+      </div>
+      <div class="card-body p-0">
+        <table class="table table-striped table-hover">
+          <thead class="thead-dark">
+            <tr class="text-center">
+              <th>Tipo de usuario</th>
+              <th>Contraseña</th>
+              <th>Acción</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr class="text-center">
+              <td>Profesor</td>
+              <td>
+                <span id="prof" @dblclick="enableField" style="width: 100%;">********* </span>
+                <input class="text-center" id="prof" type="password" name="pass" value="1234" style="display: none">
+              </td>
+              <td></td>
+            </tr>
+            <tr class="text-center">
+              <td>Tecnio</td>
+              <td>
+                <span id="tec" @dblclick="enableField" style="width: 100%;">********* </span>
+                <input class="text-center" id="tec" type="password" name="pass" value="1234" style="display: none">
+              </td>
+              <td></td>
+            </tr>
+          </tbody>
         </table>
-			</div>
-			
-		</div>
-	</div>
+      </div>
+      
+    </div>
+  </div>
 </template>
 
 <script>
-	export default {
-		name: 'configurations',
-		data() {
-			return {
+  export default {
+    name: 'configurations',
+    data() {
+      return {
         ci: ''
-			}
-		}
-	}
+      }
+    },
+    methods: {
+      enableField(e) {
+        document.querySelector('span#'+e.target.id).style.display = 'none'
+        let input = document.querySelector('input#'+e.target.id)
+        input.style.display = 'inherit'
+        input.focus()
+        input.select()
+      }
+    }
+  }
 </script>
 
-<style></style>
+<style scoped>
+  input {
+    background: white;
+    border: none;
+    border-bottom: 1px solid grey;
+    width: 50%;
+  }
+</style>
