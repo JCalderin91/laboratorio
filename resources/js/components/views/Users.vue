77aaa1af3d325394903de6f76bdd9d4cd12c4fd0
Miguel Maestre <maestre.95@gmail.com>
2019-01-30 10:31:47 -0400

    Clientes y Usuarios

--- resources/js/components/views/Users.vue
+++ resources/js/components/views/Users.vue
@@ -1,191 +1,239 @@
 <template>
-	<div class="card col-11" style="margin: 10px;">
-		<div class="card-content row">
+  <div class="card col-11" style="margin: 10px;">
+    <div class="card-content row">
+      <div class="card-title col-12 p-0">
+        <h4 v-if="!userForm">
+          Lista de los
+          <span v-if="isAdmin">usuarios</span>
+          <span v-else>técnicos</span>
+        </h4>
+        <h4 v-else-if="userForm && update">Editar usuario</h4>
+        <h4 v-else>Registro de usuarios</h4>
+      </div>
+      <div class="col-12 row" v-if="!userForm">
+        <button
+          @click.prevent="userForm = !userForm"
+          v-if="!userForm && isAdmin"
+          class="btn btn-primary text-white"
+        >
+          <i class="fas fa-plus"></i>
+        </button>
+        
+        <input type="text" class="form-control col-4 ml-auto" placeholder="Buscar...">
+        <user-list :users="users" :edit="editUser" @edit.stop="toggleForm"></user-list>
+      </div>
 
-			<div class="card-title col-12 p-0">
-				<h4 v-if="!userForm">Lista de los 
-					<span v-if="isAdmin">usuarios</span>
-					<span v-else >técnicos</span>
-				</h4>
-				<h4 v-else-if="userForm && update">Editar usuario</h4>
-				<h4 v-else>Registro de usuarios</h4>
-			</div>
-			<div class="col-12 row" v-if="!userForm">
-				<button 
-					@click.prevent="userForm = !userForm"
-					v-if="!userForm && isAdmin"
-					class="btn btn-primary text-white">
-					<i class="fas fa-plus"></i>
-				</button>
-				
-				<input type="text" class="form-control col-4 ml-auto" placeholder="Buscar...">
-				<user-list :users="users" :edit="editUser" @edit.stop="toggleForm"></user-list>
-			</div>
+      <form v-else class="col-12 row" @submit.prevent="submit">
+        <div class="form-group col-6">
+          <label for="ci">Cedula:</label>
+          <input
+            class="form-control"
+            type="text"
+            name="ci"
+            v-model="user.cedula"
+            :disabled="update"
+            placeholder="Cedula"
+            required
+          >
+        </div>
+        <div class="form-group col-6">
+          <label for="first_name">Nombres:</label>
+          <input
+            class="form-control"
+            type="text"
+            name="first_name"
+            v-model="user.nombre"
+            placeholder="Nombres"
+            required
+          >
+        </div>
+        <div class="form-group col-6">
+          <label for="last_name">Apellidos:</label>
+          <input
+            class="form-control"
+            type="text"
+            name="last_name"
+            v-model="user.apellido"
+            placeholder="Apellidos"
+            required
+          >
+        </div>
 
-			<form v-else class="col-12 row" @submit.prevent="submit">
-				<div class="form-group col-6">
-					<label for="ci">Cedula:</label>
-					<input class="form-control" type="text" name="ci" v-model="user.cedula" :disabled="update" placeholder="Cedula">
-				</div>
-				<div class="form-group col-6">
-					<label for="first_name">Nombres:</label>
-					<input class="form-control" type="text" name="first_name" v-model="user.nombre" placeholder="Nombres">
-				</div>
-				<div class="form-group col-6">
-					<label for="last_name">Apellidos:</label>
-					<input class="form-control" type="text" name="last_name" v-model="user.apellido" placeholder="Apellidos">
-				</div>
+        <div class="form-group col-6">
+          <label for="role">Rol:</label>
+          <select
+            class="form-control"
+            type="text"
+            name="role"
+            v-model="user.esAdministrador"
+            required
+          >
+            <option value>Seleccione un rol</option>
+            <option value="true">Profesor</option>
+            <option value="false">Técnico</option>
+          </select>
+        </div>
 
-				<div class="form-group col-6">
-					<label for="role">Rol:</label>
-					<select class="form-control" type="text" name="role" v-model="user.esAdministrador">
-						<option value="">Seleccione un rol</option>
-						<option value="true">Profesor</option>
-						<option value="false">Técnico</option>
-					</select>
-				</div>
+        <div class="form-group col-6">
+          <label for="gender">Genero:</label>
+          <select class="form-control" type="text" name="gender" v-model="user.sexo" required>
+            <option value>Seleccione un genero</option>
+            <option value="F">Femenino</option>
+            <option value="M">Masculino</option>
+          </select>
+        </div>
 
-				<div class="form-group col-6">
-					<label for="gender">Genero:</label>
-					<select class="form-control" type="text" name="gender" v-model="user.sexo">
-						<option value="">Seleccione un genero</option>
-						<option value="F">Femenino</option>
-						<option value="M">Masculino</option>
-					</select>
-				</div>
-
-				<div class="col-12 float-right d-flex justify-content-end">
-					<button v-if="update" type="button" class="btn btn-secondary" @click.prevent="toggleForm" id="update-cancel-button">Cancelar</button>
-					<button v-else type="button" class="btn btn-secondary" @click.prevent="toggleForm" id="create-cancel-button">Cancelar</button>
-
-					<button id="close-modal2" type="submit" class="btn btn-primary ml-3">Guardar</button>
-				</div>
-
-			</form>
-		</div>
-	</div>
+        <div class="col-12 float-right d-flex justify-content-end">
+          <button
+            v-if="update"
+            type="button"
+            class="btn btn-secondary"
+            @click.prevent="toggleForm"
+            id="update-cancel-button"
+          >Cancelar</button>
+          <button
+            v-else
+            type="button"
+            class="btn btn-secondary"
+            @click.prevent="toggleForm"
+            id="create-cancel-button"
+          >Cancelar</button>
+          
+          <button id="close-modal2" type="submit" class="btn btn-primary ml-3">Guardar</button>
+        </div>
+      </form>
+    </div>
+  </div>
 </template>
 
 <script>
-	import UserList from '../partials/UserList'
-	export default {
-		name: 'users',
-		data() {
-			return {
-				userForm: false,
-				update: false,
-				users: [],
-        user: {
-          cedula: '',
-          nombres: '',
-          apellido: '',
-          esAdministrador: '',
-          sexo: '',
-        }
-			}
-		},
-		computed: {
-			isAdmin() {
-				return this.$session.get('isAdmin')
-			}
-		},
-		mounted() {
-			this.getUsers()
-		},
-		methods: {
-			toggleForm(event) {
-				let id = event.target.id
+import UserList from "../partials/UserList";
+export default {
+  name: "users",
+  data() {
+    return {
+      userForm: false,
+      update: false,
+      users: [],
+      user: {
+        cedula: "",
+        nombres: "",
+        apellido: "",
+        esAdministrador: "",
+        sexo: ""
+      }
+    };
+  },
+  computed: {
+    isAdmin() {
+      return this.$session.get("isAdmin");
+    }
+  },
+  mounted() {
+    this.getUsers();
+  },
+  methods: {
+    toggleForm(event) {
+      let id = event.target.id;
 
-				if (id == 'update-cancel-button') {
-					this.$emit('prompt', {
-						title: '¡Atención!',
-						message: 'Los cambios realizados seran descartados.',
-						confirmHandler: () => {
-							this.userForm = false
-							this.update = false
-							this.clearUserData()
-						}
-					})
-				} else if (id == 'create-cancel-button') {
-					this.userForm = false
-				}
-			},
+      if (id == "update-cancel-button") {
+        this.$emit("prompt", {
+          title: "¡Atención!",
+          message: "Los cambios realizados seran descartados.",
+          confirmHandler: () => {
+            this.userForm = false;
+            this.update = false;
+            this.clearUserData();
+          }
+        });
+      } else if (id == "create-cancel-button") {
+        this.userForm = false;
+      }
+    },
 
-			editUser(id) {
-				this.update = true
-				this.userForm = true
-				this.user = this.users.filter(user => user.cedula == id)[0]
-			},
+    editUser(id) {
+      this.update = true;
+      this.userForm = true;
+      this.user = this.users.filter(user => user.cedula == id)[0];
+    },
 
-			getUsers() {
-				axios
-					.get("/api/users")
-					.then(response => {this.users = response.data.data})
-					.catch(error => {
-						this.$emit('error', error)
-					});
-			},
-			createUser() {				
-				axios
-					.post('api/users', this.user)
-					.then(response => {
-						Swal({
-              type: 'success',
-              title: 'Excelente',
-              text: 'Datos guardados con exito',
-              confirmButtonText: 'Continuar',
-            }).then(() => {
-            	this.clearUserData()
-            	this.userForm = false
-            })
-					})
-					.catch(error => {
-						this.$emit('error', error)
-					})
-			},
-			updateUser() {
-				this.user.esAdministrador = (this.user.esAdministrador == 'true' || this.user.esAdministrador == true) ? 'true' : 'false'
-				axios
-					.put('api/users/'+this.user.identificador, this.user)
-					.then(response => {
-						Swal({
-              type: 'success',
-              title: 'Excelente',
-              text: 'Datos guardados con exito',
-              confirmButtonText: 'Continuar',
-            }).then(() => {
-            	this.clearUserData()
-            	this.userForm = false
-            })
-					})
-					.catch(error => {
-						this.$emit('error', error)
-					})
-			},
-			submit() {
-				if (this.update) {
-					this.updateUser()
-				} else  {
-					this.createUser()
-				}
+    getUsers() {
+      this.$emit("loading-data", true);
+      axios
+        .get("/api/users")
+        .then(response => {
+          this.users = response.data.data;
+          this.$emit("loading-data", false);
+        })
+        .catch(error => {
+          this.$emit("error", error);
+          this.$emit("loading-data", false);
+        });
+    },
+    createUser() {
+      axios
+        .post("api/users", this.user)
+        .then(response => {
+          Swal({
+            type: "success",
+            title: "Excelente",
+            text: "Datos guardados con exito",
+            confirmButtonText: "Continuar"
+          }).then(() => {
+            this.clearUserData();
+            this.userForm = false;
+          });
+        })
+        .catch(error => {
+          this.$emit("error", error);
+        });
+    },
+    updateUser() {
+      this.user.esAdministrador =
+        this.user.esAdministrador == "true" || this.user.esAdministrador == true
+          ? "true"
+          : "false";
+      axios
+        .put("api/users/" + this.user.identificador, this.user)
+        .then(response => {
+          Swal({
+            type: "success",
+            title: "Excelente",
+            text: "Datos guardados con exito",
+            confirmButtonText: "Continuar"
+          }).then(() => {
+            this.clearUserData();
+            this.userForm = false;
+          });
+        })
+        .catch(error => {
+          this.$emit("error", error);
+        });
+    },
+    submit() {
+      if (this.update) {
+        this.updateUser();
+      } else {
+        this.createUser();
+      }
 
-				this.getUsers()
-			},
-			clearUserData() {
-				this.user = {
-          ci: '',
-          first_name: '',
-          last_name: '',
-          role: '',
-          gender: '',
-          password: ''
-				}
-			}
-		},
-		components: {
-			UserList
-		}
-	}
+      this.getUsers();
+    },
+    clearUserData() {
+      this.user = {
+        ci: "",
+        first_name: "",
+        last_name: "",
+        role: "",
+        gender: "",
+        password: ""
+      };
+    }
+  },
+  components: {
+    UserList
+  }
+};
 </script>
 
 <style></style>
